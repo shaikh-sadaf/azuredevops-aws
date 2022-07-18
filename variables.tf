2018b4c640829ef7feae5dca22ae5a753facf9b1
shaikh-sadaf <83399744+shaikh-sadaf@users.noreply.github.com>
2022-07-18 18:55:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,21 +26,21 @@ variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance" 
     default     = "ami-020db2c14939a8efb" 
 }
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
-}
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
-}
+ #variable "versioning" {
+  #  type        = bool
+   # description = "(Optional) A state of versioning."
+    #default     = true
+#}
+#variable "acl" {
+ #   type        = string
+  #  description = " Defaults to private "
+   # default     = "private"
+#}
+#variable "bucket_prefix" {
+ #   type        = string
+  #  description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+   # default     = "my-s3bucket-"
+#}
 variable "tags" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."
